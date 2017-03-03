ef786aae87d33373a9e6a0723742c0ba9a467158
johnrhampton <johnrhampton@gmail.com>
2017-03-02 23:16:24 -0500

    adding mongo props

--- Models/JournalEntry.cs
+++ Models/JournalEntry.cs
@@ -1,12 +1,17 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace dot_net_st_pete_api.Models
 {
     public class JournalEntry
     {
-        public int ID { get; set; }
+        public ObjectId Id { get; set; }
+        [BsonElement("Name")]
         public string Name { get; set; }
+        [BsonElement("SampleDate")]
         public DateTime SampleDate { get; set; }
-        public string Notes { get; set; }
+        [BsonElement("SampleNotes")]
+        public string SampleNotes { get; set; }
     }
 }
