254bf464a0c9718b4ac252f9468599b7c51498cc
johnrhampton <johnrhampton@gmail.com>
2017-03-02 22:32:27 -0500

    clean up

--- Controllers/JournalController.cs
+++ Controllers/JournalController.cs
@@ -1,10 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-using Microsoft.AspNetCore.Authorization;
 
 namespace dot_net_st_pete_api.Controllers
 {
