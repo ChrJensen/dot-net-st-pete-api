f84f8b2cd79d959a0b63dc7ec98551c1df46425f
johnrhampton <johnrhampton@gmail.com>
2017-03-02 22:27:46 -0500

    token generation and validation

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using dot_net_st_pete_api.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,11 +9,16 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace dot_net_st_pete_api
 {
     public class Startup
     {
+        // secret key used to generate jwt - normally stored in environment vars
+        private const string SECRET_KEY = "N8P2b9z??jI'y8rNxLA8e3lzGU37dF";
+        private readonly SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -30,13 +35,25 @@ namespace dot_net_st_pete_api
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            // Make authentication compulsory across the board (i.e. shut down EVERYTHING unless explicitly opened up).
+            services.AddOptions();
+
+            // lock down all routes
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
                                  .RequireAuthenticatedUser()
                                  .Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            // Configure jwt issuer options
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
             });
         }
 
@@ -46,7 +63,52 @@ namespace dot_net_st_pete_api
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // todo: explore middleware approach
+            // app.UseSimpleTokenProvider(new JwtIssuer
+            // {
+            //     Path = "/login",
+            //     Audience = "ExampleAudience",
+            //     Issuer = "ExampleIssuer",
+            //     SigningCredentials = "signingCredentials",
+            //     IdentityResolver = GetIdentity
+            // });
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = buildTokenValidationParameters(),
+                AuthenticationScheme = JwtBearerDefaults.AuthenticationScheme,
+            });
+
             app.UseMvc();
+        }
+
+        private TokenValidationParameters buildTokenValidationParameters()
+        {
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            return new TokenValidationParameters
+            {
+                // validate the keys
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+
+                // validate the token issuer (iss)
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                // validate the token audience (aud)
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                // validate the token is not expired (exp)
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                // specify any clock drift allowance - we set to zero
+                ClockSkew = TimeSpan.Zero
+            };
         }
     }
 }
