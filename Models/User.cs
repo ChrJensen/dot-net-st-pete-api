bac514514799622b198820d0ccc974346cdc9c92
johnrhampton <johnrhampton@gmail.com>
2017-03-03 22:57:07 -0500

    adding Id field

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,12 @@
+using MongoDB.Bson;
+
 namespace dot_net_st_pete_api.Models
 {
-  public class User
-  {
-    public string Email { get; set; }
-    public string Password { get; set; }
-  }
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
 }
