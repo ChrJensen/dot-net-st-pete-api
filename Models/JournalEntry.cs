74828377169c21f188f4ead80fa720d25fb98887
johnrhampton <johnrhampton@gmail.com>
2017-03-03 22:56:59 -0500

    updating to BeerName

--- Models/JournalEntry.cs
+++ Models/JournalEntry.cs
@@ -7,8 +7,8 @@ namespace dot_net_st_pete_api.Models
     public class JournalEntry
     {
         public ObjectId Id { get; set; }
-        [BsonElement("Name")]
-        public string Name { get; set; }
+        [BsonElement("BeerName")]
+        public string BeerName { get; set; }
         [BsonElement("SampleDate")]
         public DateTime SampleDate { get; set; }
         [BsonElement("SampleNotes")]
