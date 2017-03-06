5d6913ee3f8e558878382100dd1b42e4c7430e1f
johnrhampton <johnrhampton@gmail.com>
2017-03-05 21:27:58 -0500

    add attributes

--- Models/JournalEntry.cs
+++ Models/JournalEntry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -6,10 +7,14 @@ namespace dot_net_st_pete_api.Models
 {
     public class JournalEntry
     {
+        [BsonId]
         public ObjectId Id { get; set; }
+        [Required]
         public string Brewery { get; set; }
+        [Required]
         [BsonElement("BeerName")]
         public string BeerName { get; set; }
+        [Required]
         [BsonElement("SampleDate")]
         public DateTime SampleDate { get; set; }
         [BsonElement("SampleNotes")]
--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,11 @@
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace dot_net_st_pete_api.Models
 {
     public class User
     {
+        [BsonId]
         public ObjectId Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
